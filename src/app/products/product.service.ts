06fdc5640c6e9a48061e9bd78d0c445a326824fc
Amanda Limón <amandalimon@outlook.com>
2024-03-19 14:36:52 -0600

    read only

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,7 @@ import { Product } from "./product.model";
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  // data.id = '3893'
+  // data.createdAt = new Date(),
   products.push(data);
 }
